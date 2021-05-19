377ccb9d94b8839cd550e0d6ec35e11bab39027f
redxp <redxpertus1@gmail.com>
2021-05-19 17:08:11 +0300

    webIntro hw v01 + travis check

--- src/main/java/mate/academy/controller/IndexController.java
+++ src/main/java/mate/academy/controller/IndexController.java
@@ -1,16 +1,16 @@
 package mate.academy.controller;
 
+import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 public class IndexController extends HttpServlet {
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException {
+            throws ServletException, IOException {
         request.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(request, response);
     }
 }
