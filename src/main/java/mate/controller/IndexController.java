a45e2acd7c83aa8269b1a06dfb25e02cdfc9dbea
oleksandr-shulha <shulgalex@gmail.com>
2021-05-20 11:42:15 +0300

    checkstyle

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServletResponse;
 
 public class IndexController extends HttpServlet {
     @Override
-    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(req, resp);    }
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(req, resp);
+    }
 }
