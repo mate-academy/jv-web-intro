6bec38ddbf8fa97663977ba6ca7b9984f5c7ad59
redxp <redxpertus1@gmail.com>
2021-05-20 09:43:40 +0300

    webIntro hw v02 made work with Roman comments

--- src/main/java/mate/academy/controller/IndexController.java
+++ src/main/java/mate/academy/controller/IndexController.java
@@ -7,7 +7,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class IndexController extends HttpServlet {
-
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
