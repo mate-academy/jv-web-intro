55e56b93731060108626f37492e5a3bb288d87c9
Borys Skvortsov <Dhakmar@gmail.com>
2021-07-18 16:23:32 +0300

    init comment

---
+++ src/main/java/mate/controller/TestController.java
@@ -0,0 +1,15 @@
+package mate.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TestController extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
+            IOException {
+        req.getRequestDispatcher("/WEB-INF/views/testPage.jsp").forward(req, resp);
+    }
+}
