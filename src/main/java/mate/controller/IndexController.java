927902f58f053371a4538ce79dd4d677839999de
KonstantinIWTB <www.kosta.kon@gmail.com>
2021-12-06 16:16:31 +0200

    Empty line was removed.

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -7,7 +7,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class IndexController extends HttpServlet {
-
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
