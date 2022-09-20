7472c40bf36e0496378bc0b0f290413e5e389bd6
Serhii Buria <sergeiburya@gmail.com>
2022-09-20 21:03:58 +0300

    verified maven tests

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -1,11 +1,11 @@
 package mate.controller;
 
+import java.io.IOException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 public class IndexController extends HttpServlet {
     @Override
