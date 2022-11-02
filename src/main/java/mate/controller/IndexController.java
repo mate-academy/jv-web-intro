03ba410c5af42f4194ff65ccb358fe1a48d10d49
BP2117575 <marjanavv@gmail.com>
2022-11-02 11:56:39 +0100

    try

---
+++ src/main/java/mate/controller/IndexController.java
@@ -0,0 +1,19 @@
+package mate.controller;
+
+import com.sun.net.httpserver.HttpServer;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class IndexController extends HttpServlet {
+    @Override
+   public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+        req.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(req, resp);
+    }
+
+}
