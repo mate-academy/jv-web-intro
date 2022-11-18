74233d69a7a69c1a6041d47acc273dd6dd89bb32
Oleksiy Masaltsev <a.o.masaltsev@gmail.com>
2022-11-18 23:05:20 +0200

    dependency, servlet, jsp

---
+++ src/main/java/mate/controller/IndexController.java
@@ -0,0 +1,14 @@
+package mate.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class IndexController extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+}
