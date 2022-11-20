95ff87ae3bf625cfc9ae7403783cd76b9956a275
Yaroslav Pelekhach <108753784+Yaroslav781@users.noreply.github.com>
2022-11-20 20:15:29 +0200

    added index.jsp

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -1,2 +1,15 @@
-package mate.controller;public class IndexController {
+package mate.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class IndexController extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
 }
