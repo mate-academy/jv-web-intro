28761ccb4d07dfadfb6c9cc8ada13c8c32a3a940
Yaroslav Pelekhach <108753784+Yaroslav781@users.noreply.github.com>
2022-11-21 12:44:02 +0200

    created IndexController

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -1,13 +1,14 @@
 package mate.controller;
 
+import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 public class IndexController extends HttpServlet {
     private static final String PATH = "/WEB-INF/views/index.jsp";
+
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
