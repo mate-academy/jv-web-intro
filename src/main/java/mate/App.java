13a4284753913a6d57340d6accd8d5f33733b3ff
AndriySichniy <sichniy.andriy@gmail.com>
2022-12-12 17:04:53 +0200

    added controller

--- src/main/java/mate/App.java
+++ src/main/java/mate/App.java
@@ -1,5 +1,7 @@
 package mate;
 
+import mate.controller.IndexController;
+
 public class App {
     public static void main(String[] args) {
         // test your code here
--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -9,6 +9,6 @@ import java.io.IOException;
 public class IndexController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("webapp/WEB-INF/index.jsp").forward(req, resp);
+        req.getRequestDispatcher("WEB-INF/views/index.jsp").forward(req, resp);
     }
 }
