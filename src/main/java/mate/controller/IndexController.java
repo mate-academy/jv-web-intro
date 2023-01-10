db18a9c6c8d64f9bab5ce9eafca143c1fc93d7c6
Kateryna Takhtai <ktakhtay22@gmail.com>
2023-01-10 19:23:07 +0100

    style changes

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 public class IndexController extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp)
-        throws ServletException, IOException {
-        req.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(req, resp);
+            throws ServletException, IOException {
+        req.getRequestDispatcher("WEB-INF/views/index.jsp").forward(req, resp);
     }
 }
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -1,7 +1,7 @@
-<%@ page contentType="text/html;charset=UTF-8" %>
+<%@page contentType="text/html; charset=UTF-8" %>
 <html>
 <head>
-    <title>index page</title>
+    <title>Home Work - Hello world</title>
 </head>
 <body>
 <h1>Hello World</h1>
