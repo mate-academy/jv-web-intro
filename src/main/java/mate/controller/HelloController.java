9e290c1566949b742171139499e619595e98a5c4
Maksim Surzhik <kabaneri15@gmail.com>
2023-01-23 17:53:36 +0200

    ready

---
+++ src/main/java/mate/controller/HelloController.java
@@ -0,0 +1,14 @@
+package mate.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloController extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/views/hello.jsp").forward(req, resp);
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/hello.jsp
@@ -0,0 +1,19 @@
+<%@ page contentType="text/html;charset=UTF-8" %>
+<html>
+<head>
+    <title>Hello page</title>
+</head>
+<body>
+    <h1 style="color: chocolate;font-size: 300%; text-align: center;">
+        Hello world!
+    </h1>
+    <style>
+        img {
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+        }
+    </style>
+    <img src="/img/img_meme.jpg" alt="Congratulations" class="img">
+</body>
+</html>
