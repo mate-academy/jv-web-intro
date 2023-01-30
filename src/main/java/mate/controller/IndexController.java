0849ec932612a30885a14d3807c99d52ce6d6557
Bohdan Halitskyi <b.a.galitskiy@gmail.com>
2023-01-30 13:57:16 +0200

    fix?

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -10,6 +10,6 @@ public class IndexController extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        req.getRequestDispatcher("src/main/webapp/WEB-INF/views/index.jsp").forward(req, resp);
+        req.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(req, resp);
     }
 }
