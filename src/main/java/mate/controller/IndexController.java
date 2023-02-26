f25658ec726b3fe528db9b686b499d3d820569d6
Watxesnavo <112902418+Watxesnavo@users.noreply.github.com>
2023-02-27 00:11:01 +0100

    done

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -1,9 +1,9 @@
 package mate.controller;
 
+import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
 import javax.servlet.http.HttpServletResponse;
 
 public class IndexController extends HttpServlet {
