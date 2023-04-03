aea84a0b312b6a8066fd34a0f70ae27de708901e
Artem Ponomarenko <artemponomar98@gmail.com>
2023-04-03 14:20:38 +0300

    fixed path to index page

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -11,6 +11,6 @@ public class IndexController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
              throws ServletException, IOException {
-        req.getRequestDispatcher("src/main/webapp/WEB-INF/views/index.jsp").forward(req, resp);
+        req.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(req, resp);
     }
 }
