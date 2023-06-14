c0fa8666b53627b799a10829ce2af5446eb1dc04
Viktoriia <reraitviktoria@gmail.com>
2023-06-14 16:33:35 +0200

    HW-jv-web-intro-14-Of-June

---
+++ src/main/java/mate/controller/IndexController.java
@@ -0,0 +1,19 @@
+package mate.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class IndexController extends HttpServlet {
+    private static final String PATH
+            = "src/main/src/main/webapp/WEB-INF/views/index.jsp";
+
+    @Override
+    public void doGet(HttpServletRequest req,
+                      HttpServletResponse resp)
+            throws ServletException, IOException {
+        req.getRequestDispatcher(PATH).forward(req, resp);
+    }
+}
