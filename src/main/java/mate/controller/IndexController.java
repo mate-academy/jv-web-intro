501adbeae14d96cf2599c4058361c6938f2e0b5d
Ros <rospsix@gmail.com>
2023-07-09 17:41:00 +0100

    fix checkstyle

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class indexController extends HttpServlet {
+public class IndexController extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
